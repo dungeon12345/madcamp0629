b4c0cdefcb5e26f35ad056cf40387530516ac95f
unknown <dungeon12345@naver.com>
2019-07-01 21:19:22 +0900

    gallery finished

--- app/src/main/java/com/example/myapplication/Frag3.java
+++ app/src/main/java/com/example/myapplication/Frag3.java
@@ -23,7 +23,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
@@ -31,13 +34,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class Frag3 extends Fragment {
 
     private static final String TAG = "Frag2";
-    private static final int REQUEST_CODE = 2415135;
+    private static final int REQUEST_CODE = 99;
     private static final int PICK_FROM_ALBUM = 1;
     private File tempFile;
     public static final int MEDIA_TYPE_IMAGE = 1;
@@ -50,15 +55,20 @@ public class Frag3 extends Fragment {
 
     public float imageViewRotation = 90;
 
-    private Button takePicBtn;
+    private  Button buttonleft ;
+    private  Button buttonright ;
     private ImageView resultView;
     private TextView imgPath;
     private Gallery customGallery;
     private String[] imgs;
     private ImageView imageView;
+    private ImageView blackBackground;
+    private ImageView falseButton;
     private ArrayList<String> images;
+    private GridView gallery;
 
     private View v;
+    private File f1;
 
     public Frag3() {
     }
@@ -66,27 +76,68 @@ public class Frag3 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);
+
     }
+
+
     GridView mGridView ;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //setup();
-        super.onCreateView(inflater, container, savedInstanceState);
 
         v = inflater.inflate(R.layout.fragment_frag3, container, false);
         Button button = v.findViewById(R.id.button);
         Button button2 = v.findViewById(R.id.button2);
-        imageView = v.findViewById(R.id.imageView);
+        Button button3 = v.findViewById(R.id.button3);
+        buttonleft = v.findViewById(R.id.buttonleft);
+        buttonright = v.findViewById(R.id.buttonright);
+
+        imageView = v.findViewById(R.id.ImageView);
+        blackBackground = v.findViewById(R.id.BlackBackground);
+        falseButton = v.findViewById(R.id.button4);
+
+        buttonleft.setEnabled(false);
+        buttonright.setEnabled(false);
+        blackBackground.setVisibility(View.GONE);
+        imageView.setVisibility(View.GONE);
+        falseButton.setVisibility(View.GONE);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyGalleryApp");
+
+                if (! mediaStorageDir.exists()){
+                    if (! mediaStorageDir.mkdirs()){
+                        Log.d("MyGalleryApp", "failed to create directory");
+                    }
+                }
+
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                try{
+                        File f1 = createImageFile();
+                        mCurrentPhotoPath = f1.getAbsolutePath();
+                        // 이미지가 저장될 파일은 카메라 앱이 구동되기 전에 세팅해서 넘겨준다.
+                        Uri uri = FileProvider.getUriForFile(getActivity(), getContext().getPackageName() + ".fileprovider", f1);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri) ;
+                        startActivityForResult(takePictureIntent, REQUEST_CODE);
+                        if(f1.length() ==0 ){
+                            f1.delete();
+                        }
+                }catch( IOException e){
+                        e.printStackTrace();
+                    }
+
+
             }
+
+
+
         });
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,28 +146,130 @@ public class Frag3 extends Fragment {
                 intent.setAction(Intent.ACTION_PICK);
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 startActivityForResult(intent, PICK_FROM_ALBUM);
+
             }
         });
 
-        GridView gallery = (GridView) v.findViewById(R.id.galleryGridView);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refresh();
+            }
+        });
 
-        gallery.setAdapter(new ImageAdapter((Activity) getContext()));
 
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setVisibility(View.GONE);
+                blackBackground.setVisibility(View.GONE);
+                falseButton.setVisibility(View.GONE);
+                gallery.setEnabled(true);
+                buttonleft.setEnabled(false);
+                buttonright.setEnabled(false);
+            }
+        });
+
+        buttonleft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Uri selectedImg_uri = Uri.parse(images.get(current_position - 1));
+                    imageView.setImageURI(selectedImg_uri);
+                    imageView.setRotation(90);
+                    blackBackground.setVisibility(View.VISIBLE);
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView.bringToFront();
+                    falseButton.setVisibility(View.VISIBLE);
+                    falseButton.bringToFront();
+                    gallery.setEnabled(false);
+                    current_position--;
+                }catch(IndexOutOfBoundsException e){
+
+                    Toast.makeText(getContext(),"This is the First Picture in the Gallery",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String uri_string = images.get(current_position + 1);
+                    Uri selectedImg_uri = Uri.parse(uri_string);
+                    imageView.setImageURI(selectedImg_uri);
+                    imageView.setRotation(90);
+                    blackBackground.setVisibility(View.VISIBLE);
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView.bringToFront();
+                    falseButton.setVisibility(View.VISIBLE);
+                    falseButton.bringToFront();
+                    gallery.setEnabled(false);
+                    current_position++;
+                }catch(IndexOutOfBoundsException e){
+
+                    Toast.makeText(getContext(),"There is no more Picture in the Gallery",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        gridView();
+
+        return v;
+    }
+
+
+    public void refresh(){
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.detach(this).attach(this).commit();
+        Toast.makeText(getContext(),"Gallery has been refreshed",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    private int current_position;
+    public void gridView(){
+
+        gallery = (GridView) v.findViewById(R.id.galleryGridView);
+        ImageAdapter imageadapter = new ImageAdapter((Activity) getContext());
+        gallery.setAdapter(imageadapter);
+        gallery.setEnabled(true);
         gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,
                                     int position, long arg3) {
-                if (null != images && !images.isEmpty())
-                    Toast.makeText(
-                            getContext(),
-                            "position " + position + " " + images.get(position),
-                            Toast.LENGTH_LONG).show();
-                ;
+                if (null != images && !images.isEmpty()){
+                    Uri selectedImg_uri = Uri.parse(images.get(position));
+                    current_position = position;
+                    imageView.setImageURI(selectedImg_uri);
+                    imageView.setRotation(90);
+                    blackBackground.setVisibility(View.VISIBLE);
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView.bringToFront();
+                    falseButton.setVisibility(View.VISIBLE);
+
+                    gallery.setEnabled(false);
+                    buttonleft.setEnabled(true);
+                    buttonright.setEnabled(true);
+                    falseButton.bringToFront();
+                }
+
+
 
             }
+
+//            @Override
+//            public boolean onItemLongClicked(RecyclerView recyclerView, int position, View v) {
+//                Toast.makeText(getContext(), "길게 눌렀구나 " + arrayListOfStudent.get(position).getStudentName(), Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//
+
+
         });
-        return v;
+
+
     }
 
 
@@ -159,40 +312,8 @@ public class Frag3 extends Fragment {
             }
         }
         else if(requestCode == REQUEST_CODE){
-            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
 
-
-
-
-            FileOutputStream outStream = null;
-
-
-
-
-
-            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "TabApp");
-
-            if (! mediaStorageDir.exists()){
-                if (! mediaStorageDir.mkdirs()){
-                    Log.d("MyCameraApp", "failed to create directory");
-                }
-            }
-
-            String path = mediaStorageDir.getAbsolutePath();
-            String foler_name = "/TabApp/";
-            String fileName = String.format("%d.jpg", System.currentTimeMillis());
-            String string_path = path+foler_name;
-
-
-            try{
-                FileOutputStream out = new FileOutputStream(string_path+ fileName);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                out.close();
-            }catch(FileNotFoundException exception){
-                Log.e("FileNotFoundException", exception.getMessage());
-            }catch(IOException exception){ Log.e("IOException", exception.getMessage());
-            }
-
+            MoveToGallery();
 //            imageView.setDrawingCacheEnabled(true);
 //            Bitmap b = imageView.getDrawingCache();
 //            MediaStore.Images.Media.insertImage(getActivity().getContentResolver(),b, MediaStore.Images.Media.TITLE, MediaStore.Images.Media.DESCRIPTION);
@@ -201,6 +322,47 @@ public class Frag3 extends Fragment {
 
 
 
+
+    }
+    final static String JPEG_FILE_PREFIX = "IMG_";
+
+    final static String JPEG_FILE_SUFFIX = ".jpg";
+
+    public  String mCurrentPhotoPath;
+    
+    private File createImageFile() throws IOException{
+
+        String timeStamp = new SimpleDateFormat( "yyyyMMdd_HHmmss").format( new Date());
+
+        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+
+        File image = File.createTempFile(
+                imageFileName,			// prefix
+
+                JPEG_FILE_SUFFIX,		// suffix
+
+                new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyGalleryApp") //directory
+        );
+
+         mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+
+
+
+    public void MoveToGallery(){
+
+        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+
+        File f = new File( mCurrentPhotoPath);
+
+        Uri contentUri = Uri.fromFile( f);
+
+        mediaScanIntent.setData( contentUri);
+
+        getActivity().sendBroadcast( mediaScanIntent);
 
     }
 
@@ -287,4 +449,7 @@ public class Frag3 extends Fragment {
             return listOfAllImages;
         }
     }
+
+
+
 }
